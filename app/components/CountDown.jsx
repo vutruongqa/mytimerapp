9234bd6e9a51b6b2bc473d76950a0fc2c24c453a
Vu Truong <vutruongqa@gmail.com>
2017-06-27 15:19:43 +0700

    add new feature

--- app/components/CountDown.jsx
+++ app/components/CountDown.jsx
@@ -1,11 +1,48 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountDownForm = require('CountDownForm');
 
 var CountDown = React.createClass({
+    getInitialState:function(){
+        return {
+            count:0,
+            countDownStatus: 'stopped'
+        };    
+    },
+    
+    //Call after prop or state updated
+    componentDidUpdate: function(prevProps, prevState){
+      if(this.state.countDownStatus !== prevState.countDownStatus){
+          switch(this.state.countDownStatus){
+              case 'started': 
+                  this.startTimer();
+                  break;
+         }
+      }  
+    },
+    
+    startTimer:function(){
+      this.timer = setInterval(() =>{
+          var newCount= this.state.count - 1;
+          this.setState({
+             count: newCount >= 0 ? newCount : 0 
+          });
+      },1000);  
+    },
+    
+    handleSetCountDown: function(seconds){
+        this.setState({
+            count:seconds,
+            countDownStatus: 'started'
+        });
+    },
+    
     render: function(){
+        var {count} = this.state;
         return(
             <div>
-                <Clock totalSeconds={129}/>
+                <Clock totalSeconds={count}/>
+                <CountDownForm onSetCountDown={this.handleSetCountDown}/>
             </div>
         
         );
---
+++ app/components/CountDownForm.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var Clock = require('Clock');
+
+var CountDownForm = React.createClass({
+    onFormSubmit: function(e){
+        e.preventDefault();
+        var strSeconds = this.refs.seconds.value;
+        if( strSeconds.match(/^[0-9]*$/) && strSeconds.length > 0){
+            this.refs.seconds.value="";
+            this.props.onSetCountDown(parseInt(strSeconds, 10));
+        }
+    },
+    render: function(){
+        return(
+            <div>
+                <form onSubmit={this.onFormSubmit} ref="form" className="countdown-form">
+                    <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+                    <button className="button expanded">
+                        Start
+                    </button>
+                    
+                </form>
+            </div>
+        
+        );
+    }
+});
+
+module.exports = CountDownForm;
